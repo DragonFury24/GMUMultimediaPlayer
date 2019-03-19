328e5379d708c18dfdd951648a64b9f9413df700
Dennis Nguyen <dennisdnguyen19@gmail.com>
2019-03-19 17:11:01 -0400

    initGrid

--- src/LoadableImage.java
+++ src/LoadableImage.java
@@ -4,6 +4,7 @@ public class LoadableImage implements Loadable, StillImage {
     int height;
 
     int[] data;
+    int[][] grid;
     public LoadableImage() {
 
     }
@@ -12,6 +13,7 @@ public class LoadableImage implements Loadable, StillImage {
         width = w;
         height = h;
         this.data = data;
+        grid = initGrid(data);
     }
 
     @Override
@@ -78,5 +80,17 @@ public class LoadableImage implements Loadable, StillImage {
 
         //number of supplied values are greater than width * height
         return data.length - 3 > data[1] * data[2];
+    }
+
+    private int[][] initGrid(int[] data) {
+        int[][] to2D = new int[data[1]][data[2]];
+
+        for (int row = 0; row < data[1]; row++) {
+            for (int col = 0; col < data[2]; col++) {
+                to2D[row][col] = data[row * data[1] + col];
+            }
+        }
+
+        return to2D;
     }
 }
