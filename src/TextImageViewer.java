b60b0695192ef3f95a0b530281e40bc807e1e414
Dennis Nguyen <dennisdnguyen19@gmail.com>
2019-03-28 22:20:18 -0400

    Switch array access order

--- src/TextImageViewer.java
+++ src/TextImageViewer.java
@@ -4,15 +4,15 @@ public class TextImageViewer extends ImageViewer {
     public TextImageViewer() {
     }
 
-    public char getChar(int i) {
+    private char getChar(int i) {
         return vals[(int)(i * (vals.length / 1000.0))];
     }
 
     @Override
     public void view(StillImage img) {
-        for (int i = 0; i < img.height(); i++) {
-            for (int j = 0; j < img.width(); j++) {
-                System.out.print(getChar(img.getPixel(j, i)));
+        for (int j = 0; j < img.height(); j++) {
+            for (int i = 0; i < img.width(); i++) {
+                System.out.print(getChar(img.getPixel(i, j)));
             }
 
             System.out.println();
