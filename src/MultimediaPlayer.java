1003979c92815ec0963841e81d95a95fe9c94cca
Dennis Nguyen <dennisdnguyen19@gmail.com>
2019-03-28 22:35:56 -0400

    load()

--- src/MultimediaPlayer.java
+++ src/MultimediaPlayer.java
@@ -1,24 +1,96 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MultimediaPlayer implements Player, Loadable {
-    public MultimediaPlayer () {}
+    private TextImageViewer textImageViewer;
+    private TextAudioPlayer textAudioPlayer;
+    private Player[]        players;
+    private Loadable[]      loaders;
+    private Loadable[]      media;
+    private int             numMedia;
+
+    public MultimediaPlayer() {
+        players = new Player[2];
+        players[0] = new TextAudioPlayer();
+        players[1] = new TextImageViewer();
+
+        loaders = new Loadable[2];
+        loaders[0] = new LoadableAudio();
+        loaders[1] = new LoadableImage();
+
+        media = new Loadable[0];
+    }
 
     public int[] read(String filename) throws LoadException, IOException {
-        return new int[1];
+        Scanner scanner = new Scanner(new File("src/" + filename));
+        int     data[]  = new int[fileLength(filename)];
+
+
+        for (int i = 0; i < data.length; i++) {
+            try {
+                data[i] = scanner.nextInt();
+            } catch (InputMismatchException in) {
+                throw new LoadException("Improperly formatted data.");
+            }
+        }
+
+        scanner.close();
+
+        return data;
+    }
+
+    private int fileLength(String filename) throws IOException {
+        Scanner scanner = new Scanner(new File("src/" + filename));
+        int     count   = 0;
+
+        while (scanner.hasNext()) {
+            scanner.next();
+            count++;
+        }
+
+        scanner.close();
+
+        return count;
+    }
+
+    public void add(Loadable l) {
+
     }
 
     @Override
     public boolean matches(int[] data) {
+        for (Loadable m : loaders)
+            if (m.matches(data))
+                return true;
+
         return false;
     }
 
     @Override
     public Loadable load(int[] data) throws LoadException {
+        for (Loadable m : loaders)
+            if (m.matches(data)) {
+                String temp = m.getClass().toGenericString().split(" " )[2];
+                switch (m.getClass().toGenericString().split(" ")[2]) {
+                    case "LoadableAudio":
+                        return new LoadableAudio().load(data);
+                    case "LoadableImage":
+                        return new LoadableImage().load(data);
+                }
+            }
+
         return null;
     }
 
     @Override
     public boolean canPlay(Loadable l) {
+        for (Player p : players)
+            if (p.canPlay(l))
+                return true;
+
         return false;
     }
 
